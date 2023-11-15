a6d2615c402733b5f9ebda328c8fa08d3f5e732f
ginger <26461046+gingerchew@users.noreply.github.com>
2023-11-15 12:35:55 -0500

    Rewrite of plugin
    Utility functions like terminal formatting and the
    inclusiveLanguage function are moved to their
    own files
    
    the option `templateFormats` has been changed to `search` with
    the same typing
    
    this lets you query for pages instead of filtering based on file type
    
    this means rewriting how pages are processed
    now there is a separate process function that will be run
    either when the callback is run or on `afterRender`
    
    doing it `afterRender` will only happen if a search query
    exists in the options
    
    Adds more tests for the new search options
    
    the tests are toggled with comments in the _config.ts
    another area for improvement

--- mod.ts
+++ mod.ts
@@ -1,133 +1,75 @@
-import { Page, Site } from "lume/core.ts";
+import { Data, Page, PageData, Site } from "lume/core.ts";
+
+import { underlineIt, warnIt } from "./utils.ts";
+import { inclusiveLanguage } from "./inclusiveLanguage.ts";
 
 interface Options {
-    words: string[] | string;
-    templateFormats: string[];
+    words: string[];
+    search: string[];
     padding: number;
 }
 
-const terminalOptions = {
-    UOPEN: 4,
-    UCLOSE: 24,
-    YOPEN: 33,
-    YCLOSE: 39
-}
-
-const terminal = new Proxy(terminalOptions, {
-    get(target, key:keyof typeof terminalOptions):string {
-        return `\u001B[${target[key]}m`;
-    }
-});
-
-const warnIt = (msg:string) => {
-    const {
-        YOPEN,
-        YCLOSE
-    } = terminal;
-
-    return YOPEN + msg + YCLOSE;
-}
-
-/** Adapted from chalk */
-const underlineIt = (word: string) => {
-    const {
-        UOPEN,
-        UCLOSE
-    } = terminal;
-    return UOPEN + word + UCLOSE;
-}
-
-const makeRegex = (word:string) => new RegExp(`\\b(${word})\\b`, 'gi');
-
-function inclusiveLanguage(content: string, words:string[], padding:number) {
-    const found = [];
-
-    for (let word of words) {
-        word = word.trim();
-
-        const regex = makeRegex(word);
-
-        if (content.match(regex)) {
-            const split = content.split(regex);
-
-            for (let i = 0, l = split.length; i + 1 < l; i += 2) {
-                /** Found message should be 30 characters, the word underlined, then 30 more characters */
-                found.push(split[i].substr(-padding) + warnIt(underlineIt(word)) + split[i+2].substring(0, padding));
-            }
-        }
-    }
-
-    return found;
-}
-export const defaultWords = "simply,obviously,basically,of course,clearly,just,everyone knows,however,easy".split(',')
+export const defaultWords = ["simply","obviously","basically","of course","clearly","just","everyone knows","however","easy"];
 
 const defaults = {
     words: defaultWords,
-    templateFormats: ['md'],
+    search: [],
     padding: 30,
 };
 
-function InclusiveLanguagePlugin(options?: Partial<Options>) {
-    const opts = {
-        ...defaults,
-        ...options
-    };
-
-    let words:string[];
-    if (Array.isArray(opts.words)) {
-        words = opts.words
-    } else if (typeof opts.words === 'string') {
-        words = opts.words.split(',');
+function process(page:Page | Data<PageData>, { words, padding }:Pick<Options, 'words' | 'padding'>) {
+    // There has to be a way to do this better
+    if ((page as Data<PageData>).page) {
+        const tmpPage = (page as Data<PageData>).page;
+        page = tmpPage as Page;
     }
     
-    if (!Array.isArray(opts.templateFormats)) throw new Error('opts.templateFormats must be a type string[]');
+    if (page.content instanceof Uint8Array) {
+        throw new Error('InclusiveLanguage plugin does not support Uint8Array. Know how to implement it? Reach out at gingerchew/lume-plugin-inclusive-language');
+    }
+    
+    const content = page.content as string;
+    const url = page.src.path;
+    if (!url) return;
+    
+    const found = inclusiveLanguage(content, words, padding);
 
-    opts.templateFormats = opts.templateFormats.map(fmt => fmt[0] !== '.' ? '.'+fmt : fmt);
+    if (found.length) {
+        /** If anything is found, list them in the console */
+        console.warn(warnIt(`${underlineIt('Inclusive Language Linter')}: (${url})`));
+        console.warn('    '+found.join('\n'+'    '));
+    }
+}
+
+
+function InclusiveLanguagePlugin(options?: Partial<Options>) {
+    let {
+        words,
+        search, 
+        padding
+    } = {
+        ...defaults,
+        ...options
+    } as Options;
+
+    if (!Array.isArray(words) && typeof words === 'string') {
+        const wordsStr = words as string;
+        words = wordsStr.split(',');
+    }
+    
+    if (!Array.isArray(search)) throw new Error('opts.search must be a type string[]');
+
 
     return (site:Site) => {
-        
-        site.process(opts.templateFormats, (page:Page) => {
-            
-            
-            if (page.content instanceof Uint8Array) {
-                throw new Error('inclusiveLanguage plugin does not support Uint8Array. Know how to implement it? Reach out at gingerchew/lume-plugin-inclusive-language');
-            }
-            
-            const content = page.content as string;
-            
-            /**
-             * @TODO Fix the file format checking
-             * 
-             * since page.src.path doesn't *necessarily* include
-             * the file format of the source file, the method 
-             * used in 11ty's version of InclusiveLanguage
-             * won't run at all
-             * 
-             * For now, this is bypassed by just passing
-             * true by default
-             */
-            let shouldCheck = true;
-            const url = page.src.path;
-            if (!url) return;
-            
-            for (const format of opts.templateFormats) {
-                if (url.indexOf(format) !== -1) {
-                    shouldCheck = true;
-                    break;
+        if (search.length) {
+            site.addEventListener('afterRender', () => {
+                for (const query of search) {
+                    site.searcher.pages(query).forEach((page) => process(page, { words, padding }));
                 }
-            }
-            
-            if (!shouldCheck) return;
-
-            const found = inclusiveLanguage(content, words, opts.padding);
-
-            if (found.length) {
-                /** If anything is found, list them in the console */
-                console.warn(warnIt(`${underlineIt('Inclusive Language Linter')}: (${url})`));
-                console.warn('    '+found.join('\n'+'    '));
-            }
-
-        })
+            })
+        } else {
+            site.process('*', (page:Page) => process(page, { words, padding }));
+        }
     }
 }
 
